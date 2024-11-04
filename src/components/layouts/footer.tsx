2b635a33a2527839bb85475080c6b1155fc89ed2
pawsitive-dav <thevisualwebsite@gmail.com>
2024-11-04 10:17:57 +0700

    Update

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -60,17 +60,21 @@ export default function Footer() {
     { icon: <MailIcon className='w-5 h-5' />, label: clinicContact.email },
   ]
 
+  const copyrightText = 'Copyright © 2024 The Visual Clinic All Rights Reserved.'
+
   return (
     <footer className='bg-[#483E3B] text-white pt-14 text-lg md:text-sm'>
-      <div className='container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8'>
-        <div className='lg:col-span-2 text-[#E7DDD3] space-y-4'>
-          <Image src={logoTextWhite} alt='The Visual Clinic Logo' width={200} />
-          <h3 className='text-4xl dancing-script-font'>
-            {activeLocale === 'th' ? footerCaption.caption_th : footerCaption.caption_en}
-          </h3>
+      <div className='container px-4 md:px-6 grid grid-cols-12 gap-4 md:gap-6'>
+        <div className='col-span-12 md:col-span-6 xl:col-span-4'>
+          <div className='text-[#E7DDD3] space-y-4'>
+            <Image src={logoTextWhite} alt='The Visual Clinic Logo' width={200} />
+            <h3 className='text-3xl dancing-script-font'>
+              {activeLocale === 'th' ? footerCaption.caption_th : footerCaption.caption_en}
+            </h3>
+          </div>
         </div>
 
-        <div>
+        <div className='col-span-12 md:col-span-6 xl:col-span-3 mt-8 md:mt-0'>
           <h2 className='text-lg text-[#B8977F] mb-6'>
             {activeLocale === 'th' ? 'Social Media' : 'Social Media'}
           </h2>
@@ -95,7 +99,7 @@ export default function Footer() {
           </ul>
         </div>
 
-        <div>
+        <div className='col-span-12 md:col-span-6 xl:col-span-2 mt-8 xl:mt-0'>
           <h2 className='text-lg text-[#B8977F] mb-6'>{activeLocale === 'th' ? 'เมนู' : 'Menu'}</h2>
           <div className='grid grid-cols-2 gap-4'>
             <ul className='space-y-4 text-white/80'>
@@ -119,24 +123,24 @@ export default function Footer() {
           </div>
         </div>
 
-        <div>
-          <h2 className='text-lg text-[#B8977F] mb-6'>
-            {activeLocale === 'th' ? 'ติดต่อเรา' : 'Contact Us'}
-          </h2>
-          <ul className='space-y-4 text-white/80'>
-            {contactInfo.map((item, index) => (
-              <li key={index} className='flex items-center gap-3'>
-                {item.icon} {item.label}
-              </li>
-            ))}
-          </ul>
+        <div className='col-span-12 md:col-span-6 xl:col-span-3 mt-8 xl:mt-0'>
+          <div className='xl:w-fit mx-auto'>
+            <h2 className='text-lg text-[#B8977F] mb-6'>
+              {activeLocale === 'th' ? 'ติดต่อเรา' : 'Contact Us'}
+            </h2>
+            <ul className='space-y-4 text-white/80'>
+              {contactInfo.map((item, index) => (
+                <li key={index} className='flex items-center gap-3'>
+                  {item.icon} {item.label}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
 
       <div className='border-t border-[#665942] mt-8 py-6 text-center'>
-        <p className='text-xs text-white/40'>
-          Copyright © 2024 The Visual Clinic All Rights Reserved.
-        </p>
+        <p className='text-xs text-white/40'>{copyrightText}</p>
       </div>
     </footer>
   )
